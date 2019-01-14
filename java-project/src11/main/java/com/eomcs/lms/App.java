b74d400cc620863fcd072887879fbec1a6fccb06
ppappikko <sanghyun.dev@gmail.com>
2019-01-14 11:30:08 +0900

    .

--- java-project/src11/main/java/com/eomcs/lms/App.java
+++ java-project/src11/main/java/com/eomcs/lms/App.java
@@ -7,54 +7,57 @@ import com.eomcs.lms.handler.MemberHandler;
 
 public class App {
 
-  static Scanner keyboard = new Scanner(System.in);
+  public static Scanner keyboard = new Scanner(System.in);
+  public final static int LENGTH = 2;
 
   public static void main(String[] args) {
-
-    LessonHandler.keyboard = keyboard;
-    MemberHandler.keyboard = keyboard;
-    BoardHandler.keyboard = keyboard;
 
     while (true) {
       String command = prompt();
 
+      // 조건문을 사용하여 명령어에 따라 처리를 분기
       if (command.equals("/lesson/add")) {
         LessonHandler.addLesson();
-
-      } else if (command.equals("/lesson/list")) {
-        LessonHandler.listLesson();
 
       } else if (command.equals("/member/add")) {
         MemberHandler.addMember();
 
-      } else if (command.equals("/member/list")) {
-        MemberHandler.listMember();
-
       } else if (command.equals("/board/add")) {
         BoardHandler.addBoard();
+
+      } else if (command.equals("/lesson/list")) {
+        LessonHandler.listLesson();
+
+      } else if (command.equals("/member/list")) {
+        MemberHandler.listMember();
 
       } else if (command.equals("/board/list")) {
         BoardHandler.listBoard();
 
       } else if (command.equals("quit")) {
-        System.out.println("안녕!");
+        System.out.println("안녕!!!");
         break;
-
       } else {
-        System.out.println("실행할 수 없는 명령입니다.");
+        System.out.println("실행할 수 없는 명령입니다...");
       }
-
-      System.out.println(); // 결과 출력 후 빈 줄 출력
-    }
+      // if 조건문에 따라 처리 된 후에 공백 한 줄 
+      System.out.println();
+    } // while문 종료
 
     keyboard.close();
+
   }
 
   private static String prompt() {
+    // 사용자로부터 명령을 입력받는 부분
     System.out.print("명령> ");
-    String command = keyboard.nextLine().toLowerCase();
-    return command;
+    return keyboard.nextLine().toLowerCase();
   }
+
+
+
+
+
 
 
 
---
+++ java-project/src11/main/java/com/eomcs/lms/handler/BoardHandler.java
@@ -0,0 +1,49 @@
+package com.eomcs.lms.handler;
+
+import java.sql.Date;
+import java.util.Scanner;
+import com.eomcs.lms.App;
+import com.eomcs.lms.domain.Board;
+
+public class BoardHandler {
+
+
+  static Board[] boards = new Board[App.LENGTH];
+  static int boardIdx = 0;
+
+  public static void addBoard() {
+
+    if (boardIdx == App.LENGTH) {
+      System.out.println("더 이상 추가할 수 없습니다..!");
+    } else {
+
+      Scanner keyboard = App.keyboard;
+
+      Board board = new Board();
+
+      System.out.print("번호? ");
+      board.no = Integer.parseInt(keyboard.nextLine());
+
+      System.out.print("내용? ");
+      board.contents = keyboard.nextLine();
+
+      board.createdDate = new Date(System.currentTimeMillis()); 
+
+      board.viewCount = 0;
+
+      boards[boardIdx] = board;
+      boardIdx++;      
+
+      System.out.print("\n저장하였습니다!");
+    }
+
+  }
+
+  public static void listBoard() {
+    for (int j = 0; j < boardIdx; j++) {
+      System.out.printf("%3d, %-20s, %s, %d\n", 
+          boards[j].no, boards[j].contents, boards[j].createdDate, boards[j].viewCount);
+    }
+  }
+
+}
