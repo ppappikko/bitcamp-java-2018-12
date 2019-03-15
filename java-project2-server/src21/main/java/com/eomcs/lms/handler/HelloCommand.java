315a2ea90efa4ee93d701a5a758733afaf56a02c
ppappikko <sanghyun.dev@gmail.com>
2019-03-15 12:51:30 +0900

    서버에 IoC 컨테이너 설정

---
+++ java-project2-server/src21/main/java/com/eomcs/lms/handler/HelloCommand.java
@@ -0,0 +1,15 @@
+package com.eomcs.lms.handler;
+
+import com.eomcs.lms.context.Component;
+import com.eomcs.lms.context.RequestMapping;
+
+@Component
+public class HelloCommand{
+
+  @RequestMapping("/hello")
+  public void hello(Response response) {
+    response.println("안녕하세요!");
+
+  }
+
+}
