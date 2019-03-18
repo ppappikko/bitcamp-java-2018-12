1a7f9f85e3ed6bdd3a43445ffb2305b37a9901c7
ppappikko <sanghyun.dev@gmail.com>
2019-03-18 17:35:15 +0900

    Spring IoC 컨테이너 사용법 기초

---
+++ java-basic/src/main/java/ch29/d/Test01.java
@@ -0,0 +1,21 @@
+// 프로퍼티 값 설정 - 세터 호출
+package ch29.d;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test01 {
+  
+  public static void main(String[] args) {
+    
+    ApplicationContext iocContainer =
+        new ClassPathXmlApplicationContext("ch29/d/application-context-01.xml");
+    
+    System.out.println("-----------------------------------------");
+    
+    System.out.println(iocContainer.getBean("c1"));
+    System.out.println(iocContainer.getBean("c2"));
+    System.out.println(iocContainer.getBean("c3"));
+    System.out.println(iocContainer.getBean("c4"));
+  }
+}
